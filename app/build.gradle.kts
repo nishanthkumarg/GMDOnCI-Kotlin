1d53d110461106fc2383e94177b45383250df76f
nishanthkumarg <nishanthkumarg@google..com>
2024-05-21 11:20:33 -0700

    changes in the gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    @Suppress("UnstableApiUsage")
     testOptions {
         managedDevices{
              localDevices {
