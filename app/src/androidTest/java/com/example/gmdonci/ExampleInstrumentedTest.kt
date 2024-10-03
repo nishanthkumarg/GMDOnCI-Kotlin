77cf7650443dba3e9c9cf2e397a16fbd2e9ad767
nishanthkumarg <nishanthkumarg@google..com>
2024-10-03 14:52:33 -0700

    Revert "Update UI and remove failing tests"
    This reverts commit a5a8d5e3

--- app/src/androidTest/java/com/example/gmdonci/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/gmdonci/ExampleInstrumentedTest.kt
@@ -74,5 +74,34 @@ class ExampleInstrumentedTest {
         assert(true)
     }
 
+    @Test
+    fun failingTest9(){
+        assert(true)
+    }
+
+    @Test
+    fun failingTest10(){
+        assert(true)
+    }
+
+    @Test
+    fun failingTest11(){
+        assert(true)
+    }
+
+    @Test
+    fun failingTest12(){
+        assert(true)
+    }
+
+    @Test
+    fun failingTest13(){
+        assert(true)
+    }
+
+    @Test
+    fun failingTest14(){
+        assert(true)
+    }
 
 }
